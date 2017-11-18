58f3689c31f82e6ad96c21d70194c08f33adf877
CarleenRossing <c.rossing@hotmail.com>
2017-11-18 12:42:14 +0100

    tests

--- Afvink6.py
+++ Afvink6.py
@@ -1,95 +1,58 @@
-# Naam:
-# Datum:
-# Versie:
-
-# Voel je vrij om de variabelen/functies andere namen te geven als je die logischer vind.
-
-# Opmerking: Het alpaca bestand is erg groot! Neem eerst een klein proefstukje van het bestand, met 5 tot 10 fasta's.
-# Ga je runnen met het echte bestand, geef je programma dan even de tijd.
-
 def main():
-    bestand = "GCF_000164845.2_Vicugna_pacos-2.0.2_rna.fna" # Voer hier de bestandsnaam van het juiste bestand in, of hernoem je bestand
-    """
-    Hier onder vind je de aanroep van de lees_inhoud functie, die gebruikt maakt van de bestand variabele als argument.
-    De resultaten van de functie, de lijst met headers en de lijst met sequenties, sla je op deze manier op in twee losse resultaten.
-    """
-    headers, seqs = lees_inhoud(bestand)
+    try:
+        bestand = "GCF_000164845.2_Vicugna_pacos-2.0.2_rna.fna" # Voer hier de bestandsnaam van het juiste bestand in, of hernoem je bestand
+        #bestand = "okeoke.txt"
+        headers, seqs = lees_inhoud(bestand)
     
-
-# schrijf hier de rest van de code nodig om de aanroepen te doen
-   
-                
-    for index in range(len(headers)):                           #leest het bestand en kijkt of het zoekwoord er in staat
-        print(index)                                            #Als het zoekwoord er in staat, kijkt die of het DNA is of niet
-        if zoekwoord in headers[index]:                         #Dan kijkt die of de restrictieenzymen er zijn.
-            print(headers[index])
-            if is_dna(seqs[index]):
-                print("sequence", index, "is DNA")
-                knipt(seqs[index])
-                
-            else:
-                print("is geen DNA")
-                
-            
+        zoekwoord = input("Geef een zoekwoord op: ")
     
-    
-
-    
+        for index in range(len(headers)):                       #lees per header het bestand door
+            if zoekwoord in headers[index]:                     #als het zoekwoord in de header zit, print de headers in de lijst
+                print(headers[index])
+                if is_dna(seqs[index]):
+                    print("sequence", index + 1, "is DNA")      #print dat het DNA is
+                    knipt(seqs[index])                          #kijk of de enzymen knippen
+                else:
+                    print("het is geen dna")
+    except KeyboardInterrupt:
+        print("User interrupted")
 def lees_inhoud(bestand):
     try:
-        bestand = open(bestand)
+        bestand = open(bestand)                             #open het bestand en maak 2 lege lijsten aan
     except IOError:
-        print("het bestandsnaam bestaat niet")
+        print("Bestand niet gevonden")
     headers = []
     seqs = []
-
     sequentie = ''
-    
-    for line in bestand:
-        if line.startswith (">"):                           #Leest elke regel en kijkt of het een header is of niet
-            headers.append(line)                            #Als het niet een header is zet het in de lijst van sequenties
-            if not sequentie == '':
+    for line in bestand:                                    #voor elke lijn in het bestand, als de lijn begint met > , stop de header in de lijst headers.
+        if line.startswith (">"):
+            headers.append(line)
+            if not sequentie == '':                         #als het niet begint met de header, stop de regels in de lijst sequenties.
                 seqs.append(sequentie)
                 sequentie = ''
         else:
-            sequentie = sequentie + line.strip('\n')
-            
-            
+            sequentie = sequentie + line.strip('\n')            
     seqs.append(sequentie)
+        
+    return headers, seqs                                    #return de gevonden lijsten
 
-    #print(headers, seqs, sequentie)
-    
-  
-     
-    return headers, seqs                                #Het script returnt de nieuwe lijsten
-
-    
 def is_dna(seq):
-
-    
-    for letter in seq:                                      #Als de sequentie deze letters bevat, is het een DNA
+    for letter in seq:                                      #als in de sequentie A T G C er allemaal in zit, dan is het dna. anders niet
         if letter not in ['A','T','C','G']:
             return False
-        
     return True
     
 
 def knipt(seq):
-
     try:
-        enzymen = open("enzymen.txt", 'r')
+        enzymen = open("enzymen.txt", 'r')                      #open het bestand van de restrictie enzymen
     except IOError:
-        print("het bestand bestaat niet")
-
-
-    for regel in enzymen:                                   #leest het bestand in en haalt allemaal stomme tekens weg
-        regel = regel.replace("^", "")                      #Bekijkt of de enzymen in het grote bestand staan
+        print("Bestand niet gevonden")
+    for regel in enzymen:
+        regel = regel.replace("^", "")
         regel = regel.replace("\n", "")
-        enzyme, knip = regel.split()
+        enzyme, knip = regel.split()                        #haal de tekens er uit en splits de enzymnaam en de sequentie van elkaar
         if knip in seq:
-            print(enzyme + " knipt in " + seq)
+            print(enzyme + " knipt in " + knip)             #print welk enzym knipt met welke sequentie
 
-    
-    
 main()
-
